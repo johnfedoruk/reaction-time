ddbf8ac30a348ad4045112dfdd88a08acc31ed38
johnfedoruk <johnny@johnfedoruk.ca>
2018-02-20 23:00:11 -0600

    Updated main to take input and extract value and unit in a loop

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <string>
+#include <sstream>
 
 int main(int argc,char* argv[]) {
-    std::cout << "Hello world" << std::endl;
+    int value;
+    std::stringstream ss;
+    std::string input;
+    std::string unit;
+    std::cin >> input;
+    while(input.compare("q")!=0) {
+        ss << input;
+        ss >> value >> unit;
+        std::cout << "value: " << value << " unit: " << unit << std::endl;
+        ss.str("");
+        ss.clear();
+        std::cin >> input;
+    }
+    std::cout << "program terminated" << std::endl;
     return 0;
 }
